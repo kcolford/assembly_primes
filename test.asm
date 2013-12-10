9a871ac90076be9aa58e8ccd20e467c116a83a7c
Kieran Colford <colfordk@gmail.com>
2013-12-10 18:07:19 -0500

    Added a license to the program.

--- test.asm
+++ test.asm
@@ -1,12 +1,31 @@
-;;; Hey Emacs! asm-mode is awful.  Use -*- asm86-mode -*-
+;;; The prime number generator that only works on the linux x86_64
+
+;;; Copyright (C) 2013 Kieran Colford
+;;;
+;;; This program is free software: you can redistribute it and/or
+;;; modify it under the terms of the GNU General Public License as
+;;; published by the Free Software Foundation, either version 3 of the
+;;; License, or (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;; General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program.  If not, see
+;;; <http://www.gnu.org/licenses/>.
+;;;
+;;; The copyright holder can be contacted at <colfordk@gmail.com>.
+
 
 ;;; This is the assembly code for a prime number generator.  It uses a
 ;;; standtard sieve of eratosthenes to do so (running time of O(N *
 ;;; sqrt(N)) but benchmarking shows it having a linear running time.
-
+;;;
 ;;; It runs on the x86_64 microprocessor running linux.  Do not link
 ;;; using a C compiler, only use the standard `ld' command.
-
+;;;
 ;;; The GNU assembler defines the comment character for the x86_64 as
 ;;; being a `#' symbol.  This is very problematic and unconventional
 ;;; and causes problems for my editor.  So you should preprocess this
